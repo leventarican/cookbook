6dec9e1954c903a920d7802ef834d44776ad8633
leventarican <levent@protonmail.com>
2019-08-02 23:22:45 +0200

    working with csv

--- python/csv_benchmark.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('1000000salesrecords.csv', newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    linecount = 0
-    for row in csvreader:
-        linecount += 1
-    print('csvfile lines: {}'.format(linecount))
