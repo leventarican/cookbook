4e627fc357bdde958d5aba017e10cc5cef57c202
leventarican <levent@protonmail.com>
2019-08-14 23:57:02 +0200

    log configuration

--- java/java-lang/src/main/java/com/github/leventarican/LoggingJUL.java
+++ java/java-lang/src/main/java/com/github/leventarican/LoggingJUL.java
@@ -1,6 +1,7 @@
 package com.github.leventarican;
 
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 /**
@@ -12,14 +13,27 @@ public class LoggingJUL {
     private static final Logger log = Logger.getLogger(LoggingJUL.class.getName());
 
     public void exec() {
+        configuration();
+
         log.info("simple demo of Logging-API (JSR-47)");
 
         try {
             ((Object)null).toString();
         } catch(Exception e) {
+            // same as log.severe() ...
             log.log(Level.SEVERE, "you tried null to String", e);
         }
 
         log.info("no problem we catch'ed that error.");
+    }
+
+    void configuration() {
+        // -Djava.util.logging.config.file=logging.properties
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+        try {
+            LogManager.getLogManager().readConfiguration();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
