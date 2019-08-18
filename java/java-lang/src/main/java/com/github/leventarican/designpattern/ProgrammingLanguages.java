9a5d39d5b5e74bfaf64cdc407541d9683bd53bf0
leventarican <levent@protonmail.com>
2019-08-18 23:34:59 +0200

    design pattern: singleton

---
+++ java/java-lang/src/main/java/com/github/leventarican/designpattern/ProgrammingLanguages.java
@@ -0,0 +1,12 @@
+package com.github.leventarican.designpattern;
+
+/**
+ * Singleton pattern used on enum
+ */
+public enum ProgrammingLanguages {
+    INSTANCE;
+    public String kotlin() { return "kotlin"; }
+    public String kotlinVersion() { return "v 1.3.0"; }
+    public String java() { return "java"; }
+    public String javaVersion() { return "v 12"; }
+}
