3c5bfd454d98ea09de671a64c2ec9f0262c48bea
leventarican <levent@protonmail.com>
2019-08-20 22:38:13 +0200

    just finish the pattern

--- java/java-lang/src/main/java/com/github/leventarican/designpattern/MessagesOrchestrator.java
+++ java/java-lang/src/main/java/com/github/leventarican/designpattern/MessagesOrchestrator.java
@@ -1,5 +1,29 @@
 package com.github.leventarican.designpattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessagesOrchestrator {
+    
+    private List<MessageListener> listeners = new ArrayList<>();
+    private List<MessageEvent> events = new ArrayList<>();
+
+    public MessagesOrchestrator() {
+        events.add(new MessageEvent(this, "java"));
+        events.add(new MessageEvent(this, "kotlin"));
+        events.add(new MessageEvent(this, "cpp"));
+    }
+    
+    public void addListener(MessageListener listener) {
+        listeners.add(listener);
+    }
+    
+    public void exec() {
+        for (MessageListener listener : listeners) {
+            for (MessageEvent event : events) {
+                listener.code(event);
+            }
+        }
+    }
     
 }
--- java/java-lang/src/main/java/com/github/leventarican/designpattern/TheObserverPattern.java
+++ java/java-lang/src/main/java/com/github/leventarican/designpattern/TheObserverPattern.java
@@ -2,6 +2,15 @@ package com.github.leventarican.designpattern;
 
 public class TheObserverPattern {
     public void exec() {
-
+        class Foobar implements MessageListener {
+            @Override
+            public void code(MessageEvent event) {
+                System.out.println(Foobar.class.getName() + " received event: " + event.getMessage());
+            }
+        }
+        
+        MessagesOrchestrator orchestrator = new MessagesOrchestrator();
+        orchestrator.addListener(new Foobar());
+        orchestrator.exec();
     }
 }
