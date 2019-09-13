ec6982e1f4cbce28bb4236e5e1ee964367d3951e
leventarican <levent@protonmail.com>
2019-09-13 18:17:11 +0200

    lambda examples

--- python/lambda.py
+++ python/lambda.py
@@ -6,3 +6,19 @@ print(
     '''
 )
 
+def func(x,y):
+    return x+y
+f = func(3,4)
+print('function: {}'.format(f))
+
+l = lambda x,y: x+y
+print('lamda: {}'.format(l(3,4)))
+
+def hello():
+    prefix = 'hello'
+    result = (lambda name: prefix + ' ' + name)
+    return result
+
+foo = hello()
+print(foo)
+print(foo('bar'))
