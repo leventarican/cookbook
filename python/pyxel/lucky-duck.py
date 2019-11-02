23b44c1178854bb811be6e2981da33ebc88ab6e1
leventarican <levent@protonmail.com>
2019-11-02 11:53:29 +0100

    add sin movement

--- python/pyxel/lucky-duck.py
+++ python/pyxel/lucky-duck.py
@@ -1,21 +1,26 @@
 from random import randint
-
 import pyxel
+import math
 
+PLAYER_Y_DEFAULT = 100
 
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Pyxel Jump")
 
-        pyxel.load("assets/luckyduck0.pyxres")
+        pyxel.load("assets/luckyduck1.pyxres")
 
         self.score = 0
         self.player_x = 16
-        self.player_y = 16*6
+        self.player_y = PLAYER_Y_DEFAULT
         self.player_vy = 0
         self.player_jump = False
         self.player_is_alive = True
-        self.player_gravity = 0
+        
+        self.player_gravity = 9
+
+        self.delta_time = 0
+        self.stop_time = 0
 
         self.fruit = [(i * 60, randint(0, 104), randint(0, 2), True) for i in range(4)]
 
@@ -27,6 +32,8 @@ class App:
 
         # print(pyxel.height)   = 120
         # print(pyxel.width)    = 160
+
+        self.unique = (50, 50)
 
         pyxel.run(self.update, self.draw)
 
@@ -41,22 +48,20 @@ class App:
 
     def update_player(self):
         if pyxel.btn(pyxel.KEY_SPACE):
-            self.player_jump = True
-
-        print(self.player_gravity)
-
+            if self.player_jump == False:
+                self.player_jump = True
+                self.delta_time = pyxel.frame_count
         if self.player_jump:
-            self.player_gravity += min(self.player_gravity+1, 16)
-
-        if self.player_y <= 40:
-            self.player_gravity -= max(0, self.player_gravity+1)
-            self.player_jump = False
-
-        self.player_y -= self.player_gravity
-
-        if self.player_y > 96:
-            self.player_y = 96
-            self.player_gravity = 0
+            if (pyxel.frame_count - self.delta_time) < 10:
+                self.player_y -= self.player_gravity
+            else:
+                self.player_jump = False
+        else:
+            if self.player_y is not PLAYER_Y_DEFAULT:
+                self.player_y += self.player_gravity
+                if self.player_y > PLAYER_Y_DEFAULT:
+                    self.player_y = PLAYER_Y_DEFAULT
+                self.player_jump = False
 
     def update_fruit(self, x, y, kind, is_active):
         if is_active and abs(x - self.player_x) < 12 and abs(y - self.player_y) < 12:
@@ -93,12 +98,15 @@ class App:
             if is_active:
                 pyxel.blt(x, y, 0, 32 + kind * 16, 0, 16, 16, 12)
 
+        # draw unique
+        pyxel.blt(math.sin(pyxel.frame_count*0.09%90)*90, 30, 0, 80, 0, 16, 16, 12)
+
         # draw player
         pyxel.blt(
             self.player_x,
             self.player_y,
             0,
-            16 if self.player_gravity > 0 else 0,
+            16 if self.player_jump == True else 0,
             0,
             16,
             16,
