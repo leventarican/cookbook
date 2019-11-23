53ed7cb5e0f04bc37e9b4270d6b0e895cceec0f6
leventarican <levent@protonmail.com>
2019-11-23 23:15:53 +0100

    collections, generics, ...

--- java/java-lang/src/main/java/com/github/leventarican/Collections.java
+++ java/java-lang/src/main/java/com/github/leventarican/Collections.java
@@ -18,15 +18,81 @@ public class Collections {
     void filter() {
         String[] input = {"java", "kotlin", "Java", "c++"};
         var programmingLanguages = Arrays.asList(input);
+        
         var result = programmingLanguages.stream()
                 .filter(pl -> pl.equalsIgnoreCase("java"))
                 .collect(Collectors.joining(";"));
         
         System.out.println(result);
+        
+        int[] numbers = {1, 200, 3, 40};
+        
+//        Arrays.sort(numbers, (x,y) -> {
+//            return x.compareTo(y);
+//        });
+        
+        for (String programmingLanguage : programmingLanguages) {
+            System.out.println(programmingLanguage);
+        }
+    }
+    
+    void generic() {
+//        Generics sind nicht kovariant, sie sind invariant. 
+        Number x = Integer.valueOf(3);
+        Number y = Double.valueOf(3.3);
+        
+//        Typ Double[] ist vom Typ Number[]: das nennt sich kovariant.
+        Number[] numbers = new Double[2];
+//        numbers[0] = 3;
+        numbers[1] = 3.3;
+        
+//        wildcard typ ?: ein unbekannter typ
+        Computer<Java> j = new Java(10);
     }
     
     public static void main(String[] args) {
         var app = new Collections();
         app.filter();
+        app.generic();
+    }
+}
+
+/**
+ * excursion in generics
+ * 
+ * ?: Wildcard-Typ
+ * ? extends Typ: Upper-bounded Wildcard-Typ
+ * ? super Typ: Lower-bounded Wildcard-Typ
+ */
+
+class Computer<T extends ProgrammingLanguage> {
+    private T value;
+    public Computer(T value) {
+        this.value = value;
+    }
+    public T get() {
+        return this.value;
+    }
+}
+
+class ProgrammingLanguage {
+    private int id;
+    public ProgrammingLanguage(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+}
+
+class Java extends ProgrammingLanguage {
+    public Java(int id) {
+        super(id);
+    }
+}
+
+class Kotlin extends Java {
+    public Kotlin(int id) {
+        super(id);
     }
 }
