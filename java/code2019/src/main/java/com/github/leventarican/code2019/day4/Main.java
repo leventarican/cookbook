878aa06d68a47b29ad39ccf5962f55e06fd1d65a
leventarican <levent@protonmail.com>
2019-12-11 23:34:10 +0100

    finetuning

--- java/code2019/src/main/java/com/github/leventarican/code2019/day4/Main.java
+++ java/code2019/src/main/java/com/github/leventarican/code2019/day4/Main.java
@@ -1,30 +1,45 @@
 package com.github.leventarican.code2019.day4;
 
+import java.util.stream.IntStream;
+
 /**
+ * How many different passwords meet the criteria?
+ * 
  * @author Levent
  */
 public class Main {
     
-    String a = "122345";
+    String[] input = {
+        "122345",
+        "111111",
+        "111123",
+        "223450",
+        "123789",
+        "1237789"
+    };
     
-    void draft() {
-        var meets_criteria = true;
-        var numbers = a.chars()
+    boolean solution(String password) {
+        if (password.length() != 6) {
+            return false;
+        }
+        var numbers = password.chars()
                 .map(c -> Character.getNumericValue(c))
                 .toArray();
         for (int i = 0; i+1 < numbers.length;) {
             var current = numbers[i++];
             var next = numbers[i];
             if (current > next) {
-                meets_criteria = false;
-                break;
+                return false;
             }
         }
-        System.out.println("meet criteria: " + meets_criteria);
+        return true;
     }
     
     public static void main(String[] args) {
         var app = new Main();
-        app.draft();
+        var result = IntStream.range(0, app.input.length)
+                .filter(i -> app.solution(app.input[i]))
+                .count();
+        System.out.println(result);
     }
 }
