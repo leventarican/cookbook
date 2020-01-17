bbe019037cb8d5aa2ebd21b8fb681a1badcfad4f
leventarican <levent@protonmail.com>
2020-01-18 00:38:19 +0100

    TestNG groups, dependencies, ...

--- java/testng/simple/src/main/java/com/github/leventarican/simple/Main.java
+++ java/testng/simple/src/main/java/com/github/leventarican/simple/Main.java
@@ -1,14 +1,42 @@
 package com.github.leventarican.simple;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * @author Levent
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("testng");
-        
+
         Stream.of(args).forEach((arg) -> System.out.println(arg));
+
+        int[] numbers = IntStream.of(8, 3, 4).toArray();
+//        Arrays.binarySearch()
+        int index = binarySearch(numbers, 3);
+
+        System.out.println("index: " + index);
+    }
+
+    public static int binarySearch(int[] numbers, int number) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = numbers[mid];
+            if (guess == number) {
+                return mid;
+            }
+            if (guess > number) {
+                high = mid - 1;
+            } else {
+                high = mid + 1;
+            }
+        }
+
+        return -1;
     }
 }
--- java/testng/simple/src/test/java/com/github/leventarican/simple/MainNGTest.java
+++ java/testng/simple/src/test/java/com/github/leventarican/simple/MainNGTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 /**
+ * Create/Update Test in NetBeans with: Main.java > Tools > Create/Update Tests
  *
  * @author Levent
  */
@@ -15,13 +16,45 @@ public class MainNGTest {
     /**
      * Test of main method, of class Main.
      */
-    @Test
+    @Test(enabled = false)
     public void testMain() {
         System.out.println("main");
         String[] args = {
             "Junit", "TestNG"
         };
         Main.main(args);
+    }
+    
+    @Test(groups = "regression")
+    public void regressionTest0() {
+        assertEquals(0, 0);
+    }
+    
+    @Test(groups = "regression")
+    public void regressionTest1() {
+        assertEquals(1, 2);
+    }
+    
+    /**
+     * depends on groups regression:
+     * if regression test all successful then this will be executed.
+     */
+    @Test(dependsOnGroups = "regression")
+    public void approvalTest() {
+        assertEquals(1, 1);
+    }    
+
+    /**
+     * Test of binarySearch method, of class Main.
+     */
+    @Test(enabled = true)
+    public void testBinarySearch() {
+        System.out.println("binarySearch");
+        int[] numbers = {5, 3, 8};
+        int number = 3;
+        int expResult = 1;
+        int result = Main.binarySearch(numbers, number);
+        assertEquals(result, expResult);
     }
     
 }
