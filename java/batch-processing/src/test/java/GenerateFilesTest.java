0beaecdaddd6f51a0ca68b740be2b3348bc1a577
leventarican <levent@protonmail.com>
2020-02-13 23:39:31 +0100

    nothing important

--- java/batch-processing/src/test/java/GenerateFilesTest.java
+++ java/batch-processing/src/test/java/GenerateFilesTest.java
@@ -23,10 +23,10 @@ public class GenerateFilesTest {
         Set<String> expected = Set.of("20200213", "20200214", "20200215");
         try {
             /*
-            1. list file in test-env/
+            1. list file in WORKING_DIR
             2. filter out sample.file
             3. convert path to string
-            4. remove prefix test-env/
+            4. remove prefix WORKING_DIR
             5. test match
             */
             boolean doesMatch = Files.list(testEnvironment)
@@ -36,7 +36,7 @@ public class GenerateFilesTest {
                     .allMatch(expected::contains);
             assertTrue(doesMatch);
         } catch (IOException ex) {
-            System.out.println(ex); // e.g. java.nio.file.NoSuchFileException: test-env
+            System.out.println(ex); // e.g. java.nio.file.NoSuchFileException: WORKING_DIR
         }
     }    
 }
