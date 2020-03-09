3f489b806650e6bd46b432d7db5b35e1bf0adf4f
leventarican <levent@protonmail.com>
2020-03-09 23:58:17 +0100

    Java Code Coverage

---
+++ java/jacoco/src/main/java/com/github/leventarican/jacoco/Main.java
@@ -0,0 +1,8 @@
+package com.github.leventarican.jacoco;
+
+/**
+ * @author Levent
+ */
+public class Main {
+
+}
---
+++ java/jacoco/src/test/java/com/github/leventarican/jacoco/MainTest.java
@@ -0,0 +1,11 @@
+package com.github.leventarican.jacoco;
+
+import org.junit.Test;
+
+public class MainTest {
+    
+    @Test
+    public void testStep() {
+        
+    }
+}
