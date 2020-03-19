d206f9a95bae5b02777f290d42c992a82797b147
leventarican <levent@protonmail.com>
2020-03-19 23:59:29 +0100

    walktree java

--- java/java-lang/src/main/java/com/github/leventarican/Files.java
+++ java/java-lang/src/main/java/com/github/leventarican/Files.java
@@ -1,7 +1,12 @@
 package com.github.leventarican;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Levent
@@ -9,33 +14,46 @@ import java.nio.file.Path;
 public class Files {
 
     void exec() {
-        tree();
+//        tree();
+        attributes();
+    }
+
+    private void attributes() {
+        try {
+            String dir = ".";
+            Path start = Path.of(dir);
+            java.nio.file.Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(file.getFileName() + ": " + attrs.lastModifiedTime());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 
     /**
-     * will print something like this: 
-     * 
-     * src
-     *  main
-     *   java
-     *    com
-     *     github
-     *      leventarican
+     * will print something like this:
+     *
+     * src main java com github leventarican
      */
     static void tree() {
         try {
             String dir = ".";
             Path root = Path.of(dir);
             int depth = root.getNameCount();
-            
+
             java.nio.file.Files.walk(root, 10)
                     .filter(p -> p.toFile().isDirectory())
                     .map(p -> "  ".repeat(p.getNameCount() - depth)
-                            .concat(p.getFileName().toString()))
+                    .concat(p.getFileName().toString()))
                     .forEach(System.out::println);
-            
+
         } catch (IOException ex) {
             System.out.println(ex);
         }
     }
+
 }
