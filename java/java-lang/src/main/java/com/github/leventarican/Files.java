54a74a6f3cf89f5acaba9b8f0373c551b2a90ecb
leventarican <levent@protonmail.com>
2020-03-20 01:03:27 +0100

    another walktree example

--- java/java-lang/src/main/java/com/github/leventarican/Files.java
+++ java/java-lang/src/main/java/com/github/leventarican/Files.java
@@ -5,6 +5,7 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,7 +16,8 @@ public class Files {
 
     void exec() {
 //        tree();
-        attributes();
+//        attributes();
+        treePro();
     }
 
     private void attributes() {
@@ -51,6 +53,41 @@ public class Files {
                     .concat(p.getFileName().toString()))
                     .forEach(System.out::println);
 
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * walk directories, skip target/ folder.
+     */
+    static void treePro() {
+        try {
+            String dir = ".";
+            Path root = Path.of(dir);
+            AtomicInteger depth = new AtomicInteger(root.getNameCount());
+            
+            java.nio.file.Files.walkFileTree(root, new SimpleFileVisitor<>() {                
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    
+                    int dirDepth = dir.getNameCount() - depth.get();
+                    
+                    if(dir.getFileName().startsWith("target"))
+                        return FileVisitResult.SKIP_SUBTREE; 
+                    else {
+                        System.out.println(" ".repeat(dirDepth).concat(dir.getFileName().toString()));
+                    }
+                    
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+                
+            });
         } catch (IOException ex) {
             System.out.println(ex);
         }
