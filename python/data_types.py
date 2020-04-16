97c28342000b4d81039072a5d8aae5c19877ec2e
leventarican <levent@protonmail.com>
2020-04-16 23:57:09 +0200

    refresh

---
+++ python/data_types.py
@@ -0,0 +1,15 @@
+# https://introcs.cs.princeton.edu/python/12types/
+
+type_int = 99
+type_float = 3.14
+type_bool = True
+type_str = 'python'
+
+print(f'int literal: {type_int}')
+print(f'float literal: {type_float}')
+print(f'bool literal: {type_bool}')
+print(f'str literal: {type_str}')
+
+print(f'int operator power: {2**2}')
+print(f'float operator exponentation: {1.1**2}')
+print(f'bool operator and: {True or False}')
