ec5a68588e6f0c33af9f33e880060e9ed52e1cea
leventarican <levent@protonmail.com>
2020-04-18 00:14:14 +0200

    queue fifp

--- python/stack_queue.py
+++ python/stack_queue.py
@@ -8,20 +8,37 @@
 # pop: an element
 class Stack:
     def __init__(self):
-        self.stack = []
+        self.a = []
     def push(self, element):
-        self.stack += [element]
+        self.a += [element]
     def pop(self):
-        self.stack.pop()
+        self.a.pop()
     def toString(self):
         s = ''
-        for element in self.stack:
+        for element in self.a:
             s = str(element) + ' ' + s
         return s
 
+class Queue(Stack):
+    def pop(self):
+        self.a = self.a[::-1]
+        self.a.pop()
+
 if __name__ == "__main__":
+    print('# Stack')
     stack = Stack()
     stack.push(3)
     stack.push(9)
+    print(stack.toString())
+    print('now last out: pop')
     stack.pop()
     print(stack.toString())
+
+    print('\n# Queue')
+    queue = Queue()
+    queue.push(3)
+    queue.push(9)
+    print(queue.toString())
+    print('now first out: pop')
+    queue.pop()
+    print(queue.toString())
