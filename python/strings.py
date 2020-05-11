46577295ca92a1788f6576c3cf1c1ed232ee3212
leventarican <levent@protonmail.com>
2020-05-11 15:30:14 +0200

    string format

--- python/strings.py
+++ python/strings.py
@@ -15,3 +15,17 @@ print(multiline_string)
 upper_lower_string = 'learn python in 60s'
 print(upper_lower_string.upper())
 print(upper_lower_string.upper().lower())
+
+# since Python 3.6
+python = 'python'
+f_string = f"f string work since {python} {3.6}"
+print(f_string)
+
+# before Python 3.6
+# %-formatting 
+print('hello %s %s' % (python, python))
+
+# str.format()
+version = 2.6
+print("hello {}".format(python))
+print("str.format introduced in {1} version {0}".format(version, python))
