eda2e65fdd3fad39c856ffc52ffd0e854cd178e3
leventarican <levent@protonmail.com>
2020-10-30 00:14:35 +0100

    include unittest module

--- python/dojo/kata0.py
+++ python/dojo/kata0.py
@@ -12,5 +12,22 @@ def xo(s):
     x = s.lower().replace("x", "")
     return len(o) == len(x)
 
-# xo('xo')
-# xo('xo0')
+# ##############################################################################
+
+import unittest
+
+# unittest will test all the methods whose name starts with test
+class SampleTest(unittest.TestCase):
+    def test_0(self):
+        self.assertTrue(xo("ooxx"))
+    def test_1(self):
+        self.assertTrue(xo("ooxXm"))
+    def test_2(self):
+        self.assertFalse(xo("zzoo"))
+
+unittest.main()
+# ...
+# ----------------------------------------------------------------------
+# Ran 3 tests in 0.000s
+
+# OK
