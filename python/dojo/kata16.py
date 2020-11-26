f71d8f2ccc4632a8b6a70632960f66560f6437a0
leventarican <levent@protonmail.com>
2020-11-26 22:00:49 +0100

    kata python

--- python/dojo/kata16.py
+++ python/dojo/kata16.py
@@ -1,13 +1,14 @@
 # https://www.codewars.com/kata/563f037412e5ada593000114/train/python
 
 def calculate_years(principal, interest, tax, desired):
-    sum_of_p = principal
-    for x in range(5):
-        t = (sum_of_p / 100) * tax
-        i = (sum_of_p / 100) * interest
-        sum_of_p = t + i
-        print(f"s {sum_of_p}; i {i}; t: {t}")
-    return 3
+    p = principal
+    y = 0
+    while p < desired:
+        y = y + 1
+        a = p * interest
+        p = p + a - a * tax
+    print(y)
+    return y
 
 if __name__ == "__main__":
     assert(calculate_years(1000, 0.05, 0.18, 1100) == 3)
