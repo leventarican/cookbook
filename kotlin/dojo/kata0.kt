a5ebe849c83ddea672de07911b37fb0fe5c030ac
leventarican <levent@protonmail.com>
2020-11-27 23:54:44 +0100

    kata in kotlin

---
+++ kotlin/dojo/kata0.kt
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/kotlin
+
+// #############################################################################
+// compile and run: kotlinc kata.kt && kotlin KataKt
+// #############################################################################
+
+fun numberToString(num: Int): String {
+    var a = "$num"
+    println(a)
+    return "$num"
+}
+
+fun main() {
+    numberToString(10)
+}
+
+// fun numberToString(num: Int) = "$num"
