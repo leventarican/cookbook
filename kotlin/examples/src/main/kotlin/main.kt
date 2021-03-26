cfc443e6e463238024b573f17cc60b9eddfda5b4
leventarican <levent@protonmail.com>
2021-03-26 23:58:35 +0100

    tuples, triples, syntax sugar

--- kotlin/examples/src/main/kotlin/main.kt
+++ kotlin/examples/src/main/kotlin/main.kt
@@ -3,4 +3,14 @@ import objectOrientedProgramming.Task
 
 fun main() {
     println("kotlin in action")
+
+    val keyValue0 = "key" to "value"
+    val keyValue1 = Pair<String, String>("kotlin", "java")
+    println("syntax sugar. same expression but concise code: ${keyValue0.first} ")
+
+    val (key, value) = keyValue0
+    println("destruct a Pair: $key")
+
+    val point = Triple(0, 1, 10)
+    println("triples: $point")
 }
