7b8dd22b41e5f70372d372219be5752e4c864c23
leventarican <levent@protonmail.com>
2021-06-07 23:54:45 +0200

    collections ...

--- kotlin/examples/src/main/kotlin/collections.kt
+++ kotlin/examples/src/main/kotlin/collections.kt
@@ -3,22 +3,30 @@ import java.util.*
 /*
 https://kotlinlang.org/docs/collections-overview.html
 https://play.kotlinlang.org/byExample/05_Collections/01_List
+https://play.kotlinlang.org/byExample/05_Collections/03_Map
+https://play.kotlinlang.org/byExample/05_Collections/11_partition
 
 demonstration of collection extension functions
  */
 
 fun collections() {
-    // list: an ordered collection. read-only or mutable
+    // list: an ordered (access by index) collection. read-only or mutable
     val a = mutableListOf(1, 2, 0)
     val b = listOf(1, 2, 0)
 
     a.add(100)
     a.forEach { i -> print(i) }
+    println("ordered collection: ${b[2]}")
 
-    // set: an unordered collection. no duplicates
+    // set: an unordered collection. no duplicates. unique elements.
     val c = setOf(1, 1, 0)
     val d = mutableSetOf(1, 1, 0)
     println(c)
+
+    // map: aka dictionary, key-value pairs
+    val e = mapOf(0 to "kotlin", 1 to "java", 2 to "rust")
+    val f = mutableMapOf(1 to 100, 2 to 200)
+    e.forEach { (k, v) -> println("key: $k; value: $v") }
 }
 
 fun ordering() {
@@ -65,8 +73,17 @@ fun transformation() {
     println(f)
 }
 
+// split collection to pairs of lists using predicate
+fun partition() {
+    val a = listOf(100, 10, 1000, 10_000, -100)
+    val (x, y) = a.partition { i -> i <= 100 }
+    x.forEach { print("x: $it ") }
+    y.forEach { print("y: $it ") }
+}
+
 fun main() {
     collections()
     ordering()
     transformation()
+    partition()
 }
