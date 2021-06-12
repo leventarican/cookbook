96fb7a22ebc3d02bbc98335b8f8378fc7e46f85b
leventarican <levent@protonmail.com>
2021-06-12 23:36:00 +0200

    kotlin extension function and properties

---
+++ kotlin/examples/src/main/kotlin/functionalProgramming/extension.kt
@@ -0,0 +1,14 @@
+package functionalProgramming
+
+// extension function and properties
+
+data class Extensible(val x: Int)
+
+fun Extensible.prettyPrint(): Unit = println("${this.javaClass.simpleName} is data class; arg x: $x; arg y: $y")
+val Extensible.y: Int get() = 100
+
+fun main() {
+    val a = Extensible(1)
+    println(a.x)
+    a.prettyPrint()
+}
