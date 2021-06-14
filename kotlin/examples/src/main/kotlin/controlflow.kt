b36a50bcbd042642b87cd77e4c1f363a151a2b8b
leventarican <levent@protonmail.com>
2021-06-14 23:10:53 +0200

    kotlin when

--- kotlin/examples/src/main/kotlin/controlflow.kt
+++ kotlin/examples/src/main/kotlin/controlflow.kt
@@ -1,9 +1,29 @@
 private data class Example(val data: String)
 
-fun main() {
+fun equalityChecks() {
     val a = Example("bits and bytes")
     val b = a.copy()
 
     println("structural comparison: ${a == b}")
     println("referential comparison: ${a === b}")
+}
+
+fun whenStatement() {
+    var a : Any
+    a = 10L
+    when(a) {
+        is String -> println("a String")
+        !is String -> println("not an String")
+    }
+
+    val c = when(1) {
+        1 -> "kotlin"
+        else -> "java"
+    }
+    println(c)
+}
+
+fun main() {
+    equalityChecks()
+    whenStatement()
 }
