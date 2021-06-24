4232a61cebecdf02f8a0a3aa2d5eeb146a314743
leventarican <levent@protonmail.com>
2021-06-24 21:16:13 +0200

    kotlin ...

--- kotlin/examples/src/main/kotlin/dojo/kata0.kt
+++ kotlin/examples/src/main/kotlin/dojo/kata0.kt
@@ -1,16 +1,34 @@
 package dojo
+
 /**
 https://www.codewars.com/kata/56484848ba95170a8000004d/train/kotlin
+https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/kotlin
  */
 
 /*
 distance from origin
  */
-fun gps(s:Int, x:DoubleArray):Int {
+fun gps(s: Int, x: DoubleArray): Int {
 
     return 0
 }
 
+fun countRedBeads(nBlue: Int): Int =
+    if (nBlue > 0) {
+        val a = (nBlue - 1) * 2
+        println("out $a")
+        a
+    } else {
+        0
+    }
+/*
+fun countRedBeads(nBlue: Int): Int = when {
+  nBlue < 2 -> 0
+  else -> (nBlue - 1) * 2
+}
+ */
+
 fun main() {
     gps(s = 100, doubleArrayOf(0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61))
+    countRedBeads(3)
 }
--- kotlin/examples/src/main/kotlin/functionalProgramming/extension.kt
+++ kotlin/examples/src/main/kotlin/functionalProgramming/extension.kt
@@ -7,8 +7,12 @@ data class Extensible(val x: Int)
 fun Extensible.prettyPrint(): Unit = println("${this.javaClass.simpleName} is data class; arg x: $x; arg y: $y")
 val Extensible.y: Int get() = 100
 
+val String.foo: Int get() = 100
+
 fun main() {
     val a = Extensible(1)
     println(a.x)
     a.prettyPrint()
+
+    println("bar".foo)
 }
