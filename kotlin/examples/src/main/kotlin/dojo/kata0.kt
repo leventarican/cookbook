a13e95cde191d092dfa419a0075ea5b1c4341256
leventarican <levent@protonmail.com>
2021-06-25 23:45:58 +0200

    kotlin kata

--- kotlin/examples/src/main/kotlin/dojo/kata0.kt
+++ kotlin/examples/src/main/kotlin/dojo/kata0.kt
@@ -3,6 +3,7 @@ package dojo
 /**
 https://www.codewars.com/kata/56484848ba95170a8000004d/train/kotlin
 https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/kotlin
+https://www.codewars.com/kata/587309155cfd6b9fb60000a0/train/kotlin
  */
 
 /*
@@ -28,7 +29,28 @@ fun countRedBeads(nBlue: Int): Int = when {
 }
  */
 
+fun moneyValue(money:String):Double {
+    println("in: $money")
+    val a = money.replace("$", "").replace(" ", "").toDoubleOrNull() ?: 0.0
+    println("out: $a")
+    return a
+}
+/*
+fun moneyValue(money:String) = money.filter { it != ' ' && it != '$' }.toDoubleOrNull() ?: 0.0
+fun moneyValue(money: String) = money.filter { it in "0123456789-." }.toDoubleOrNull() ?: 0.0
+fun moneyValue(money:String):Double {
+    var s = money.replace("[^0-9.-]".toRegex(), "").replace("^-$".toRegex(), "");
+
+    if (s.isEmpty()) {
+      return 0.0;
+    } else {
+      return s.toDouble();
+    }
+}
+ */
+
 fun main() {
     gps(s = 100, doubleArrayOf(0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61))
     countRedBeads(3)
+    moneyValue("$$$")
 }
