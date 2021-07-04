bfa95d6f7a161c26ea4b058dd8f9d907e88a38a8
leventarican <levent@protonmail.com>
2021-07-04 23:41:36 +0200

    kotlin scope

--- kotlin/examples/src/main/kotlin/scopefunctions.kt
+++ kotlin/examples/src/main/kotlin/scopefunctions.kt
@@ -6,11 +6,12 @@ scope functions: let, run, with, apply, also
 
 class Developer {
     private val programmingLanguages = mutableListOf<String>()
+    val numberOfProgrammingLanguages = programmingLanguages.size
     fun put(language: String) = programmingLanguages.add(language)
     fun code() {
         print("tech stack: ")
         programmingLanguages.forEach {
-            print("$it ")
+            print("$it; ")
         }
         println()
     }
@@ -37,4 +38,27 @@ fun main() {
         a.reversed()
     }.joinToString { "$it" }
     println(a)
+
+    val g: String? = null
+    val h = g.let {
+        println("processing")
+        it ?: "default"
+    }
+    println(h)
+
+    d.run {
+        put("scope function: run")
+    }
+    d.let {
+        it.put("scope function: let")
+    }
+
+    with(d) {
+        println("$numberOfProgrammingLanguages")
+        // instead of d.numberOfProgrammingLanguages
+    }
+
+    d.apply {
+        put("scope function: apply")
+    }.code()
 }
