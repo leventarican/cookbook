0ee6ea111b5b107cbea5a4e299b2b0f34ffa9003
leventarican <levent@protonmail.com>
2021-07-05 23:49:20 +0200

    lazyness

---
+++ kotlin/examples/src/main/kotlin/lazy.kt
@@ -0,0 +1,16 @@
+
+fun init() = "CODE"
+
+fun main() {
+    var a: String? = null
+    var b: String = "DEFAULT"
+    val c: String by lazy { init() }
+
+    // ...
+    b = init()
+    a = b
+
+    println(a)
+    println(b)
+    println(c)
+}
--- kotlin/examples/src/main/kotlin/main.kt
+++ kotlin/examples/src/main/kotlin/main.kt
@@ -1,6 +1,21 @@
 import objectOrientedProgramming.Status
 import objectOrientedProgramming.Task
 
+// memoized function: TODO
+// fun double(x: Int) = x * 2
+
+val cache = mutableMapOf<Int, Int>()
+fun double(x: Int) =
+    if (cache.containsKey(x)) {
+        cache[x]
+    } else {
+        val result = x * 2
+        cache[x] = result
+        result
+    }
+
+data class Programmer(val name: String, val exp: Int)
+
 fun main() {
     println("kotlin in action")
 
@@ -13,4 +28,10 @@ fun main() {
 
     val point = Triple(0, 1, 10)
     println("triples: $point")
+
+    val a = listOf(Programmer("java", 10), Programmer("kotlin", 20))
+    val b = a.maxOf { programmer: Programmer -> programmer.exp }
+    println(b)
+    val c = a.joinToString(" ", transform = { p: Programmer -> p.name })
+    println(c)
 }
